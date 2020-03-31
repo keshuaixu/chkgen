50598976e386b158a582ecc12c0f0f8d4fc40f8b
Keshuai Xu <urillx@gmail.com>
2020-03-31 03:08:14 -0400

    sort of works

---
+++ chkgen.js
@@ -0,0 +1,40 @@
+const arr = x => Array.from(x);
+const num = x => Number(x) || 0;
+const str = x => String(x);
+const isEmpty = xs => xs.length === 0;
+const take = n => xs => xs.slice(0, n);
+const drop = n => xs => xs.slice(n);
+const reverse = xs => xs.slice(0).reverse();
+const comp = f => g => x => f(g(x));
+const not = x => !x;
+const chunk = n => xs =>
+    isEmpty(xs) ? [] : [take(n)(xs), ...chunk(n)(drop(n)(xs))];
+
+var a = ['', 'one ', 'two ', 'three ', 'four ', 'five ', 'six ', 'seven ', 'eight ', 'nine ', 'ten ', 'eleven ', 'twelve ', 'thirteen ', 'fourteen ', 'fifteen ', 'sixteen ', 'seventeen ', 'eighteen ', 'nineteen '];
+var b = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
+function num2words(num) {
+    if ((num = num.toString()).length > 9) return 'overflow';
+    if (num === 0) return 'zero';
+    n = ('000000000' + num).substr(-9).match(/^(\d{2})(\d{2})(\d{2})(\d{1})(\d{2})$/);
+    if (!n) return;
+    var str = '';
+    str += (n[1] != 0) ? (a[Number(n[1])] || b[n[1][0]] + ' ' + a[n[1][1]]) + 'crore ' : '';
+    str += (n[2] != 0) ? (a[Number(n[2])] || b[n[2][0]] + ' ' + a[n[2][1]]) + 'lakh ' : '';
+    str += (n[3] != 0) ? (a[Number(n[3])] || b[n[3][0]] + ' ' + a[n[3][1]]) + 'thousand ' : '';
+    str += (n[4] != 0) ? (a[Number(n[4])] || b[n[4][0]] + ' ' + a[n[4][1]]) + 'hundred ' : '';
+    str += (n[5] != 0) ? ((str != '') ? 'and ' : '') + (a[Number(n[5])] || b[n[5][0]] + ' ' + a[n[5][1]]) + '' : '';
+    return str;
+}
+
+function decimal2words(decimal_str) {
+    let s = decimal_str.replace(',', '').split('.');
+    return num2words(parseInt(s[0])) + ' and ' + s[1] + '/100 dollars'
+}
+
+window.addEventListener('load', (event) => {
+    document.getElementById('dollars-amount').onkeyup = function () {
+        document.getElementById('non-arabic-dollars').innerHTML = decimal2words(document.getElementById('dollars-amount').innerText);
+    };
+});
+
